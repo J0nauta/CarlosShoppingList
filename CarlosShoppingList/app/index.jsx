fa45abd403177e906a743f78bd250ed7d5d1febd
J0nauta <jhonataromao2010@gmail.com>
2026-04-29 11:39:01 -0300

    Projetonn

---
+++ CarlosShoppingList/app/index.jsx
@@ -0,0 +1,20 @@
+import { link } from 'expo-router';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function Welcome() {
+  return (
+    <View style={styles.container}>
+    <View style={styles.containerImage}>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
