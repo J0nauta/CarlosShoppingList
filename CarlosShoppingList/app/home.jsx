2047c9740160dae104ab2e5ed2a954c1520c63fa
J0nauta <jhonataromao2010@gmail.com>
2026-05-18 07:56:22 -0300

    Meu shopping esta quase pronto

--- CarlosShoppingList/app/home.jsx
+++ CarlosShoppingList/app/home.jsx
@@ -26,19 +26,19 @@ export default function Home() {
                 <View style={styles.footer}>
                     <View style={styles.inputContainer}>
                         <TextInput
-                        color="#fff"
-                        fontSize={18}
-                        placeholder='Digite o nome do produto...'
-                        placeholderTextColor="#aeaeae"
-                                >
-                        
+                            color="#fff"
+                            fontSize={18}
+                            placeholder='Digite o nome do produto...'
+                            placeholderTextColor="#aeaeae"
+                        >
 
-                         <View>
-                            <TouchableOpacity style={styles.iconContainers} >
-                                <Ionicons name="add" size={36} color="#fff" />
-                            </TouchableOpacity>
-                         </View>
-                   </TextInput>
+
+                            <View>
+                                <TouchableOpacity style={styles.iconContainers} onPress={addProdutos}>
+                                    <Ionicons name="add" size={36} color="#fff" />
+                                </TouchableOpacity>
+                            </View>
+                        </TextInput>
                     </View>
                 </View>
             </ImageBackground>
@@ -46,4 +46,48 @@ export default function Home() {
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    header: {
+        padding: 25,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#000000c0',
+        borderBottomStartRadius: 30,
+        borderBottomEndRadius: 30,
+    },
+    title: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        color: '#ffffffff'
+    },
+    footer: {
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        backgroundColor: '#000000c0',
+        borderTopEndRadius: 30,
+        borderTopStartRadius: 30
+    },
+    inputContainer: {
+        backgroundColor: '#000',
+        elevation: 40,
+        flex: 1,
+        height: 50,
+        marginVertical: 20,
+        borderRadius: 30,
+        justifyContent: 'center',
+    },
+    iconContainers: {
+        borderRadius: 25,
+        height: 50,
+        width: 50,
+        backgroundColor: '#000',
+        elevation: 40,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
