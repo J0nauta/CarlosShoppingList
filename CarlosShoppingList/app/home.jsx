cf985449123e19c8959cb89a2277da325c949aac
J0nauta <jhonataromao2010@gmail.com>
2026-05-27 10:47:57 -0300

    prorpo

--- CarlosShoppingList/app/home.jsx
+++ CarlosShoppingList/app/home.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import { 
+import React, { useState, useEffect } from "react";
+import {
   Alert,
   FlatList,
   ImageBackground,
@@ -7,52 +7,113 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  View
-} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Ionicons } from '@expo/vector-icons';
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import ItemList from "../components/ItemList";
 
 export default function Home() {
-  const [textInput, setTextInput] = useState('');
+  const [textInput, setTextInput] = useState("");
   const [items, setItems] = useState([]);
 
   function addProduto() {
-    console.log(textInput);
-    if (textInput == '') {
+    // console.log(textInput);
+    if (textInput == "") {
       Alert.alert(
-        'Ocorreu um problema :(',
-        'Por favor, informe o nome do produto'
+        "Ocorreu um problema :(",
+        "Por favor, informe o nome do produto",
       );
       return;
     }
     const newItem = {
       id: Date.now().toString(),
       name: textInput,
-      bought: false
+      bought: false,
     };
     setItems([...items, newItem]);
-    setTextInput('');
+    setTextInput("");
+  }
+
+  function markProduto(itemId) {
+    const newItems = items.map((item) => {
+      if (item.id == itemId) {
+        return { ...item, bought: true };
+      }
+      return item;
+    });
+    setItems(newItems);
+  }
+
+  function unmarkProduto(itemId) {
+    const newItems = items.map((item) => {
+      if (item.id == itemId) {
+        return { ...item, bought: false };
+      }
+      return item;
+    });
+    setItems(newItems);
+  }
+
+  function removeProduto(itemId) {
+    Alert.alert("Excluir Produto", "Confirma a exclusão deste Produto", [
+      {
+        text: "Sim",
+        onPress: () => {
+          const newItems = items.filter((item) => item.id != itemId);
+          setItems(newItems);
+        },
+      },
+      {
+        text: "Cancelar",
+        style: "cancel",
+      },
+    ]);
+  }
+
+  function removeAll() {
+    Alert.alert("Limpar Lista?", "Confirma a exclusão de todos os produtos?", [
+      {
+        text: "Sim",
+        onPress: () => {
+          setItems([]);
+        },
+      },
+      {
+        text: "Cancelar",
+        style: "cancel",
+      },
+    ]);
   }
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
       <ImageBackground
-        source={require('../assets/background.jpg')}
-        resizeMode='repeat'
-        style={{ flex: 1, justifyContent: 'flex-start'}}
+        source={require("../assets/background.jpg")}
+        resizeMode="repeat"
+        style={{ flex: 1, justifyContent: "flex-start" }}
       >
         <View style={styles.header}>
           <Text style={styles.title}>Lista de Compras</Text>
-          <Ionicons name='trash' size={32} color="#fff"></Ionicons>
+          <Ionicons name="trash" size={32} color="#fff" onPress={removeAll}></Ionicons>
         </View>
 
         {/* Lista de compras */}
         <FlatList
-          contentContainerStyle={{ padding: 20, paddingBottom: 100, color: '#fff'}}
+          contentContainerStyle={{
+            padding: 20,
+            paddingBottom: 100,
+            color: "#fff",
+          }}
           data={items}
-          keyExtractor={(items) => items.id.toString()}
-          renderItem={({ item}) =>
-            <Text>{item.name}</Text>
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => 
+          <ItemList 
+            item={item} 
+            markItem={markProduto}
+            unmarkItem={unmarkProduto}
+            removeItem={removeProduto}
+            />
           }
         />
 
@@ -61,7 +122,7 @@ export default function Home() {
             <TextInput
               color="#fff"
               fontSize={18}
-              placeholder='Digite o nome do produto...'
+              placeholder="Digite o nome do produto..."
               placeholderTextColor="#aeaeae"
               value={textInput}
               onChangeText={(text) => setTextInput(text)}
@@ -71,54 +132,53 @@ export default function Home() {
             <Ionicons name="add" size={36} color="#fff" />
           </TouchableOpacity>
         </View>
-
       </ImageBackground>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   header: {
     padding: 25,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#000000c0',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#000000c0",
     borderBottomStartRadius: 30,
     borderBottomEndRadius: 30,
   },
   title: {
     fontSize: 26,
-    fontWeight: 'bold',
-    color: '#ffffff'
+    fontWeight: "bold",
+    color: "#ffffff",
   },
   footer: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
     paddingHorizontal: 20,
-    backgroundColor: '#000000c0',
+    backgroundColor: "#000000c0",
     borderTopStartRadius: 30,
     borderTopEndRadius: 30,
   },
   inputContainer: {
-    backgroundColor: '#000',
+    backgroundColor: "#000",
     elevation: 40,
     flex: 1,
     height: 50,
     marginVertical: 20,
     borderRadius: 30,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   iconContainer: {
     borderRadius: 25,
     height: 50,
     width: 50,
-    backgroundColor: '#000',
+    backgroundColor: "#000",
     elevation: 40,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
